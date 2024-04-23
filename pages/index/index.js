38fa6203a035a5d73447a2f99f607b06ef65b2e3
潘获帆 <13272822187@163.com>
2024-04-23 09:30:45 +0800

    feat: :tada: 完成基本抽签查看吃啥功能

---
+++ pages/index/index.js
@@ -0,0 +1,142 @@
+// index.js
+Page({
+  data: {
+    title: "看看今天吃啥呢？",
+    isTitleShow: true,
+    isBtnClick: false,
+    list: [
+      "红烧肉鹌鹑蛋",
+      "番茄土豆炖牛腩",
+      "梅菜扣肉",
+      "红烧猪蹄",
+      "红烧牛排骨",
+      "红烧肉",
+      "糖醋排骨",
+      "番茄炒蛋",
+      "酱油鸡",
+      "板栗焖鸡",
+      "蒜香鸡翅",
+      "蜜汁鸡腿",
+      "脆皮鸡翅",
+      "香煎带鱼",
+      "剁椒鱼头",
+      "红烧大黄鱼",
+      "清蒸鲈鱼",
+      "黄焖鸡",
+      "鱼香肉丝",
+      "京酱肉丝",
+      "青椒肉丝",
+      "奥尔良烤翅",
+      "小炒牛肉",
+      "可乐鸡翅",
+      "青椒酿虾滑",
+      "番茄粉丝虾滑汤",
+      "蒜蓉粉丝虾",
+      "凉拌皮蛋",
+      "柠檬鸡爪",
+      "凉拌黄瓜",
+      "粉丝蒸鲍鱼",
+    ],
+    randomItem: "疯狂星期四",
+    randomTimer: null,
+  },
+  /**
+   * 处理按钮点击事件
+   */
+  handleBtnClick() {
+    this.setData({
+      isTitleShow: false,
+    });
+    if (this.data.isBtnClick) {
+      clearInterval(this.data.randomTimer);
+      this.setData({
+        isBtnClick: false,
+      });
+      return;
+    }
+    this.setData({
+      isBtnClick: true,
+      randomTimer: setInterval(() => {
+        this.getRandomFromList();
+      }, 80),
+    });
+  },
+  /**
+   * 从列表中随机获取一个元素
+   */
+  getRandomFromList() {
+    const { list } = this.data;
+    const randomIndex = Math.floor(Math.random() * list.length);
+    const randomItem = list[randomIndex];
+    this.setData({
+      randomItem,
+    });
+  },
+  /**
+   * 跳转bilibili小程序
+   */
+  gotoBilibili() {
+    wx.setClipboardData({
+      data: this.data.randomItem,
+      success() {
+        wx.showToast({
+          title: "已复制到剪贴板",
+          icon: "none",
+          duration: 2000,
+        });
+      },
+    });
+    setTimeout(() => {
+      wx.navigateToMiniProgram({
+        appId: "wx7564fd5313d24844",
+      });
+    }, 2000);
+  },
+  /**
+   * 跳转小红书
+   */
+  gotoRedbook() {
+    wx.setClipboardData({
+      data: this.data.randomItem,
+      success() {
+        wx.showToast({
+          title: "已复制到剪贴板",
+          icon: "none",
+          duration: 2000,
+        });
+      },
+    });
+    setTimeout(() => {
+      wx.navigateToMiniProgram({
+        appId: "wxb296433268a1c654",
+      });
+    }, 2000);
+  },
+  /**
+   * 跳转美团
+   */
+  gotoMeituan() {
+    wx.setClipboardData({
+      data: this.data.randomItem,
+      success() {
+        wx.showToast({
+          title: "已复制到剪贴板",
+          icon: "none",
+          duration: 2000,
+        });
+      },
+    });
+    setTimeout(() => {
+      wx.navigateTo({
+        url: `/pages/web/web`,
+      });
+    }, 2000);
+  },
+
+  onLoad() {
+    console.log("onLoad");
+  },
+  onShow() {
+    console.log("onShow");
+  },
+});
---
+++ pages/web/web.js
@@ -0,0 +1,51 @@
+// pages/web/web.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    url: "https://market.waimai.meituan.com/gd/single.html?el_biz=waimai&el_page=gundam.loader&gundam_id=2KAWnD&activity_id=380797&utm_source=60413&utm_medium=weixin_mp&utm_campaign=other&utm_content=1782352996668616754_7&utm_term=&channel=union&mt_id=ho15Le-Rd&mt_key=1681ca2fae8a7d4161b6d731aa6f876b&click_cps_url=https%3A%2F%2Fclick.meituan.com%2Ft%3Ft%3D1%26c%3D2%26p%3D3wHixb5zgS1k",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    console.log(this.data.url);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
